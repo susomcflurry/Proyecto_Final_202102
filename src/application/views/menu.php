42e4f61d8bd276b5e672e37d1526cc27e36051d6
susomcflurry <jesuscangagalvan@gmail.com>
2021-03-22 20:42:30 +0100

    .

--- src/application/views/menu.php
+++ src/application/views/menu.php
@@ -6,12 +6,23 @@
 //Año:2021
     echo '
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+        @media (max-width: 700px) {
+            body > header > div > div.col-md-4.col-sm-0.bye{
+                display: none !important;
+            }
+            body > header > div > div.col-md-8.col-sm-12.topnav > ul > li > a{
+                padding: 0 1% !important;
+                font-size: 12px !important;
+            }
+        }
+    </style>
     <header>
         <div class="row">
-            <div class="col-4">
+            <div class="col-md-4 col-sm-0 bye">
                  <img  alt="Imagen fundacxion loyola"  src="'. base_url() .'img/download.jpg">
             </div>
-            <div class="col-8 topnav">
+            <div class="col-md-8 col-sm-12 topnav">
                 <ul>
                     <li><a href="' . base_url() . '"><span class="material-icons">search</span>Buscar recurso</a></li>
                     <li><a href="' . base_url() . 'new"><span class="material-icons">note_add</span>Nuevo Recurso</a></li>
